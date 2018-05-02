d9f976eebff5c032dc0ab81cc43ba003c7eca247
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-05-02 21:22:18 +0100

    extend repository query, refactor Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-import { Loading } from '.';
+import { Loading, RepositoryList } from '.';
 
-const GET_CURRENT_USER = gql`
+const GET_REPOSITORIES_OF_CURRENT_USER = gql`
   {
     viewer {
-      login
-      name
+      repositories(
+        first: 5
+        orderBy: { direction: DESC, field: STARGAZERS }
+      ) {
+        edges {
+          node {
+            id
+            name
+            url
+            descriptionHTML
+            primaryLanguage {
+              name
+            }
+            owner {
+              login
+              url
+            }
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+            watchers {
+              totalCount
+            }
+            viewerSubscription
+          }
+        }
+      }
     }
   }
 `;
 
 const Profile = () => {
   return (
-    <Query query={GET_CURRENT_USER}>
+    <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
       {({ data, loading }) => {
         const { viewer } = data;
 
@@ -22,12 +48,7 @@ const Profile = () => {
           return <Loading />;
         }
 
-        return (
-          <div>
-            <h1>Name: {viewer.name}</h1>
-            <h2>Username: {viewer.login}</h2>
-          </div>
-        );
+        return <RepositoryList repositories={viewer.repositories} />;
       }
       }
     </Query>
