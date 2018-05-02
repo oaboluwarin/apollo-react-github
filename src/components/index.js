a5813b7c076b44f5c36455bf38cffb6188f30ed2
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-05-02 21:21:55 +0100

    create repository-related components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export { default as Loading } from './Loading';
 export { default as Profile } from './Profile';
+export { default as Link } from './Link';
+export { default as RepositoryItem } from './RepositoryItem';
+export { default as RepositoryList } from './RepositoryList';
+
